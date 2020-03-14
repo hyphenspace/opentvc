fe04816930c03b58e9deb87d655e0a2f6521d653
akilhylton <hyltonakil@gmail.com>
2020-03-14 02:40:21 -0400

    Atmel project folder

---
+++ board/hardware/hardware.c
@@ -0,0 +1,31 @@
+#include "../drivers/hardware.h"
+
+void initStatusLED(void) {
+  DDRC |= (1 << DDC0);
+}
+
+void blueStatusLED(void) {
+  LED_PORT |= (1 << BLUE_LED);
+  _delay_ms(500);
+  LED_PORT &= ~(1 << BLUE_LED);
+  _delay_ms(250);
+}
+/*
+void redStatusLED(void) {
+  
+}
+
+void greenStatusLED(void) {
+  
+}
+*/
+
+void initBuzzer(void) {
+  DDRD |= (1 << DDD3);
+  for (int i = 0; i < 150; i++) {
+    BUZZER_PORT |= (1 <<  BUZZER);
+    _delay_us(1000);
+    BUZZER_PORT &= ~(1 << BUZZER);
+    _delay_us(1000);
+  }
+}
