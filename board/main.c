7ba3b363f359489d8b67e1c27060a6484baf2a3f
akilhylton <hyltonakil@gmail.com>
2020-09-04 02:06:34 -0400

    Updated main.c

--- board/main.c
+++ board/main.c
@@ -8,23 +8,33 @@
 #include "driver/usart.h"
 #include "driver/hardware.h"
 #include "driver/lsm9ds1.h"
-#include "driver/time.h"
 
-static FILE mystdout = FDEV_SETUP_STREAM(usartTransmit, NULL, _FDEV_SETUP_WRITE); // ONLY HERE FOR DEBUGGING. 
+static FILE mystdout = FDEV_SETUP_STREAM(print, NULL, _FDEV_SETUP_RW); // ONLY HERE FOR DEBUGGING. 
 
+//double delta_t;
+//unsigned long t;
 lsm9ds1Vector_t a;
 lsm9ds1Vector_t g;
 int main (void) {
-	usartInit(MYUBRR);
-	stdout = &mystdout;
-	setupLSM9DS1();
 	sei();
-  while(1) {
-	 //g = getGyroData();
-	 a = getAccelData();
-	 printf("a_pitch: %f, a_roll: %f\n", a.pitch, a.roll);	
+	usartInit(12);
+	stdout = &mystdout;
+	//init_micros();
+	//setupLSM9DS1();
+	//t = micros();
 
-	_delay_ms(150);
+  while(1) {
+	printf("Hello: %d\n",100);
+	//delta_t = ((double)(micros() - t) / 1000000.0);
+	//t = micros();
+	//printf("%f\n",delta_t);
+	 //g = getGyroData();
+	 //a = getAccelData();
+	 //printf("a_pitch: %f, a_roll: %f\n", a.pitch, a.roll);	
+	 //printf("a_pitch: %f, a_roll: %f\n", a.pitch, a.roll);	
+	 //printf("g_x: %f, g_y: %f\n", g.x / 100.0, g.y);	
+
+	_delay_ms(25);
 	}	
   return 0;
 }
