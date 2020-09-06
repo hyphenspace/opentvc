99650364b7d236c60e6e396f25bf2d8eee5946da
akilhylton <hyltonakil@gmail.com>
2020-09-06 19:39:14 -0400

    Updated hardware lib 🔧

--- board/driver/hardware.h
+++ board/driver/hardware.h
@@ -4,7 +4,10 @@
 #define BUZZER_PORT PORTD
 #define BUZZER PD3
 #define LED_PORT PORTC
-#define BLUE_LED PC0
+#define BLUE_LED PC1
+#define GREEN_LED PC0 
+#define RED_LED PC2
+#define BEEP_COUNT 150
 
 void blueStatusLED(void);
 void initStatusLED(void);
