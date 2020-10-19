656085cd1697227bdeec71db009dcdae04cd3935
akilhylton <hyltonakil@gmail.com>
2020-10-19 00:35:48 -0400

    Updated main.c

--- board/main.c
+++ board/main.c
@@ -7,22 +7,17 @@
 #include "driver/servo.h"
 #include "driver/usart.h"
 #include "driver/hardware.h"
-#include "driver/lsm9ds1.h"
 
 static FILE mystdout = FDEV_SETUP_STREAM(print, NULL, _FDEV_SETUP_RW); // ONLY HERE FOR DEBUGGING. 
 
-lsm9ds1Vector_t a;
-
 int main (void) {
-	    sei();
-	    usartInit(MYUBRR);
-	    stdout = &mystdout;
-		servoInit();
-		servoAttach(Y_SERVO);
-	    setupLSM9DS1();
-  	while(1) {
-		a = getAccelData();
-		servoWirteY(a.pitch);		
+    sei();
+    usartInit(MYUBRR);
+    stdout = &mystdout;
+    servoInit();
+    servoAttach(Y_SERVO);
+    while(1) {
+
     }	
     return 0;
 }
