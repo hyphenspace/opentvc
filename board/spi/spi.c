356e6007b03a9ffbbbf5b32e0ba0fb38808648d6
akilhylton <hyltonakil@gmail.com>
2020-10-19 00:00:57 -0400

    Updated spi lib and added fxas21002 support

--- board/spi/spi.c
+++ board/spi/spi.c
@@ -8,8 +8,9 @@
 
 /** Enable SPI, Master, set clock rate fck/4. */
 void spiInit(void) {
-  DDR_SPI |= ((1 << DD_SS) | (1 << DD_SS_MAG) | (1 << DD_SCK) | (1  << DD_MOSI));
+  DDR_SPI |= ((1 << DD_SS_MAG) | (1 << DD_SCK) | (1  << DD_MOSI));
   DDR_SPI &= ~(1 << DD_MISO);
+  DDRC |= (1 << DD_SS);
   SPI_PORT |= ((1 << SS) | (1 << SS_MAG));
   SPCR = ((1 << SPE) | (1 << CPHA) | (1 << CPOL) | (1 << MSTR)); 
 }
